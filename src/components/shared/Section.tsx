4389f9019db94f7a6b87bdb86565fd39eb77969c
KumJungMin <rmawjdals@naver.com>
2024-10-04 15:01:07 +0900

    1. 공통 스타일을 가진 share ui 컴포넌트로 스타일 반복을 줄일 수 있다.

--- src/components/shared/Section.tsx
+++ src/components/shared/Section.tsx
@@ -5,6 +5,10 @@ import styles from './Section.module.scss'
 
 const cx = classNames.bind(styles)
 
+/**
+ * Section 컴포넌트는 공통적으로 사용되는 디자인을 별도의 컴포넌트로 뺀 것입니다.
+ * 공통적으로 사용되는 디자인을 별도의 컴포넌트로 뺄 수 있다.
+ */ 
 function Section({
   className,
   children,
