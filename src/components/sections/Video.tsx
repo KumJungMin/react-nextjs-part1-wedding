39deddf13df85339416ae5aee496dc07934c0034
KumJungMin <rmawjdals@naver.com>
2024-10-04 15:10:58 +0900

    3. 비디오 포맷에는 webm, mp4가 있다. 호환성을 위해 2가지 포맷을 제공해야한다.

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -5,8 +5,19 @@ import Section from '../shared/Section'
 
 const cx = classNames.bind(styles)
 
+/** 
+ * 비디오에는 webm, mp4 두 가지 형식의 비디오 파일이 있습니다.
+ * webm은 구글에서 만든 비디오 포맷으로, 고화질의 비디오를 높은 압축률로 저장할 수 있습니다.
+ * 단, IE에서는 지원하지 않기에 mp4 파일도 함께 제공합니다.
+ * */ 
 function Video() {
   return (
+
+    /**
+     * - video의 autoplay와 muted
+     * video에 source를 지정하면, autoplay를 해당 자동재생되지 않습니다.
+     * 이 경우 muted 속성을 추가하면 자동재생이 가능합니다.
+     * */ 
     <Section className={cx('container')}>
       <video autoPlay loop muted poster="/assets/poster.jpg">
         <source src="/assets/main.webm" type="video/webm" />
